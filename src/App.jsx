ebcb4f8d5022166726a87a9006f5c95bbd1f8f94
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-04-04 17:47:12 +0500

    bajarildi

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(intervalId);
+            setIsRunning(false);
+            alert("Vaqt Tugadi!");
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        } else {
+          setSeconds(seconds - 1);
+        }
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning, minutes, seconds]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const pauseTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setMinutes(25);
+    setSeconds(0);
+  };
+
+  const formatTime = (time) => {
+    return time < 10 ? time : time;
+  };
+
+  return (
+    <div className="pomodoro-container">
+      <h1 className="timer">
+        {formatTime(minutes)}:{formatTime(seconds)}
+      </h1>
+      <div className="buttons">
+        {!isRunning ? (
+          <button onClick={startTimer}>Boshlash</button>
+        ) : (
+          <button onClick={pauseTimer}>Toxtatish</button>
+        )}
+        <button onClick={resetTimer}>Qaytarish</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
